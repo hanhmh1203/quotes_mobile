597d5c164f669320864182ba969eceb3b2c0a0d4
william <william.mai@scs71.com>
2024-08-11 16:08:16 +0700

    add mine

--- lib/ui/mine/mine_controller.dart
+++ lib/ui/mine/mine_controller.dart
@@ -1,3 +1,30 @@
+import 'package:get/get.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:quotes_mobile/ui/base_controller.dart';
 
-class MineController extends BaseController{}
+import '../../data/models/quote_model.dart';
+import '../../data/repositories/quote_repository.dart';
+
+class MineController extends BaseController{
+  RxList<QuoteModel> quotes = RxList();
+
+  @override
+  Future<void> onReady() async {
+    // TODO: implement onReady
+    super.onReady();
+    // loadDataQuote();
+    // parseJson();
+  }
+  Future<void> loadDataQuote() async {
+    QuoteRepository repository = Get.find();
+    var list = await repository.loadQuotesFav();
+    quotes.clear();
+    quotes.addAll(list);
+  }
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    quotes.close();
+    super.onClose();
+  }
+}
--- lib/ui/mine/mine_screen.dart
+++ lib/ui/mine/mine_screen.dart
@@ -1,17 +1,57 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 
+import '../../core/colors_type.dart';
+import '../home_screen_demo/quote_item_widget.dart';
 import 'mine_page.dart';
 
 class MineScreen extends GetView<MineController> {
   const MineScreen({super.key});
 
   @override
+  // TODO: implement controller
+  MineController get controller => Get.put(MineController());
+
+  @override
   Widget build(BuildContext context) {
+    // ('app_name').tr();
     // TODO: implement build
-    return Container(
-      child: Text('Mine'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('mine').tr(),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+              },
+              child: Text("click"),
+            ),
+            Expanded(
+              // Wrap ListView.builder with Expanded to avoid layout issues
+              child: Obx(
+                    () {
+                  print(
+                      "FavouriteScreen controller.quotes.length:${controller.quotes.length}");
+                  return ListView.builder(
+                    itemCount: controller.quotes.length,
+                    // Number of items in the list
+                    itemBuilder: (context, index) {
+                      // Get color in order from the colors list
+                      final color = colors[index % colors.length];
+                      return QuoteItemWidget(
+                        color: color,
+                        itemVM: controller.quotes[index],
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ));
   }
 }
