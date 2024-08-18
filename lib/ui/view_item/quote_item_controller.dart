6dfc8ebe9b828fbb0d6e6e2cf927eee4fe791f3b
william <william.mai@scs71.com>
2024-08-18 15:04:00 +0700

    add

--- lib/ui/view_item/quote_item_controller.dart
+++ lib/ui/view_item/quote_item_controller.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
 import 'package:get/get.dart';
+import 'package:quotes_mobile/core/log_helper.dart';
 import 'package:quotes_mobile/data/models/quote_model.dart';
 import 'package:quotes_mobile/data/repositories/quote_repository.dart';
 import 'package:quotes_mobile/ui/base_controller.dart';
+import 'package:quotes_mobile/ui/mine/mine_controller.dart';
 import 'package:quotes_mobile/ui/view_item/view_item_page.dart';
 
 class QuoteItemController extends BaseController {
@@ -15,6 +19,14 @@ class QuoteItemController extends BaseController {
     quoteVM.isFav.value = !quoteVM.isFav.value;
     await quoteRepository.saveQuoteFav(quoteVM.isFav.value, quoteVM.id);
   }
-  shareQuote() async {
+
+  shareQuote() async {}
+
+  deleteQuote(int id) async {
+    LogHelper.showLog(message: "deletequote: $id");
+    var result = await quoteRepository.deleteQuote(id);
+    LogHelper.showLog(message: "deletequote result: $result");
+    MineController mineController = Get.find();
+    mineController.loadQuotes();
   }
 }
