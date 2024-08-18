e4a0a6745d1aa0c5e3d7085a4924895cf5254b80
anphu123 <103693418+anphu123@users.noreply.github.com>
2024-08-18 14:53:08 +0700

    share_quotes

--- lib/ui/view_item/quote_item_controller.dart
+++ lib/ui/view_item/quote_item_controller.dart
@@ -3,6 +3,7 @@ import 'package:quotes_mobile/data/models/quote_model.dart';
 import 'package:quotes_mobile/data/repositories/quote_repository.dart';
 import 'package:quotes_mobile/ui/base_controller.dart';
 import 'package:quotes_mobile/ui/view_item/view_item_page.dart';
+import 'package:share_plus/share_plus.dart';
 
 class QuoteItemController extends BaseController {
   QuoteViewModel quoteVM;
@@ -16,5 +17,7 @@ class QuoteItemController extends BaseController {
     await quoteRepository.saveQuoteFav(quoteVM.isFav.value, quoteVM.id);
   }
   shareQuote() async {
+    final String quoteText = '"${quoteVM.content}" - ${quoteVM.author}';
+    await Share.share(quoteText);
   }
 }
